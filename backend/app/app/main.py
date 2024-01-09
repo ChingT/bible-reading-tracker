e3b9f4d4d979e2080270f3660ceeb08f87d09bf6
ChingT <j60626j@gmail.com>
2024-01-09 15:38:53 +0100

    Add setup logger

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -2,6 +2,7 @@
 
 import logging
 import time
+from pathlib import Path
 
 from celery.signals import after_setup_logger
 from fastapi import FastAPI
@@ -12,12 +13,7 @@ from app.api.api import api_router
 from app.core.celery_app import create_celery
 from app.core.config import settings
 
-logging.basicConfig(
-    filename="logs/app.log",
-    level=logging.INFO,
-    format="%(name)s - %(levelname)s - %(message)s",
-)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("app")
 
 
 app = FastAPI(
@@ -67,3 +63,26 @@ def setup_celery_logger(logger, *args, **kwargs):
     fh.setFormatter(formatter)
     logger.addHandler(fh)
     logger.info("logging file path: %s", fh.baseFilename)
+
+
+def set_logger():
+    logs_folder = Path("logs")
+    logs_folder.mkdir(parents=True, exist_ok=True)
+    log_path = logs_folder / "app.log"
+    logger = logging.getLogger("app")
+
+    fmt = "%(asctime)s [%(levelname)s]\t%(message)s"
+    datefmt = "%Y/%m/%d %H:%M:%S"
+    formatter = logging.Formatter(fmt, datefmt)
+
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setFormatter(formatter)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+
+set_logger()
