a41dcd4255adfe69d0bc65a1fedf7faaeb4bc757
ChingT <j60626j@gmail.com>
2024-01-09 17:42:10 +0100

    Add script to populate database with books and units data

--- backend/app/app/db/init_db.py
+++ backend/app/app/db/init_db.py
@@ -1,35 +1,13 @@
 import asyncio
-import logging
 
-from sqlmodel.ext.asyncio.session import AsyncSession
-
-from app.core.config import settings
-from app.crud.user import crud_user
 from app.db.session import SessionLocal
-from app.models.user import UserCreate
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from app.utils.populate_database import create_superuser, populate_books_units
 
 
 async def main() -> None:
     async with SessionLocal() as session:
         await create_superuser(session)
-
-
-async def create_superuser(session: AsyncSession) -> None:
-    if user := await crud_user.get_by_email(session, settings.FIRST_SUPERUSER_EMAIL):
-        logging.info("Superuser %s exists in database", user)
-        return
-
-    new_user = UserCreate(
-        email=settings.FIRST_SUPERUSER_EMAIL,
-        password=settings.FIRST_SUPERUSER_PASSWORD,
-        display_name="admin",
-    )
-    user = await crud_user.create(session, new_user, is_superuser=True)
-    await crud_user.activate(session, user)
-    logging.info("Superuser %s created", user)
+        await populate_books_units(session)
 
 
 if __name__ == "__main__":
---
+++ backend/app/app/utils/populate_database.py
@@ -0,0 +1,56 @@
+import csv
+from pathlib import Path
+
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from app import crud
+from app.core.config import settings
+from app.main import logger
+from app.models.book import BookCreate, BookEnum
+from app.models.unit import UnitCreate
+from app.models.user import UserCreate
+
+source_root = Path("app/initial_data")
+
+
+async def create_superuser(session: AsyncSession) -> None:
+    if user := await crud.user.get_by_email(session, settings.FIRST_SUPERUSER_EMAIL):
+        logger.info("Superuser %s exists in database", user)
+        return
+
+    new_user = UserCreate(
+        email=settings.FIRST_SUPERUSER_EMAIL,
+        password=settings.FIRST_SUPERUSER_PASSWORD,
+        display_name="admin",
+    )
+    user = await crud.user.create(session, new_user, is_superuser=True)
+    await crud.user.activate(session, user)
+    logger.info("Superuser %s created", user)
+
+
+async def populate_books_units(session: AsyncSession) -> None:
+    csv_file_path = source_root / "bible_books.csv"
+    with Path.open(csv_file_path, encoding="utf-8") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data = dict(**row)
+
+            if not (book := await crud.book.get_by_name(session, data["full_name"])):
+                data_in = BookCreate(
+                    full_name=data["full_name"],
+                    short_name=data["short_name"],
+                    book_type=BookEnum(data["book_type"]),
+                )
+                book = await crud.book.create(session, data_in)
+                logger.info("Book %s created", book)
+
+            for chapter in range(1, int(data["num_chapters"]) + 1):
+                if not (
+                    unit := await crud.unit.get_by_book_chapter(
+                        session, book_id=book.id, chapter=chapter
+                    )
+                ):
+                    unit = await crud.unit.create(
+                        session, UnitCreate(book_id=book.id, chapter=chapter)
+                    )
+                    logger.info("Unit %s created", unit)
