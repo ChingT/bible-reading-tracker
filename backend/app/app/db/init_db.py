74145dce85ad10eb4b124d2850a40107889ee9e0
ChingT <j60626j@gmail.com>
2024-01-16 13:42:29 +0100

    Fix populate_schedules with new schedule

--- backend/app/app/db/init_db.py
+++ backend/app/app/db/init_db.py
@@ -2,18 +2,14 @@ import asyncio
 
 from app.db.session import SessionLocal
 from app.utils.create_schedules import populate_schedules
-from app.utils.populate_database import (
-    create_superuser,
-    populate_books_units,
-    populate_plans,
-)
+from app.utils.populate_database import create_superuser, populate_books, populate_plans
 
 
 async def main() -> None:
     async with SessionLocal() as session:
         await create_superuser(session)
-        await populate_books_units(session)
         await populate_plans(session)
+        await populate_books(session)
         await populate_schedules(session)
 
 
--- backend/app/app/utils/create_schedules.py
+++ backend/app/app/utils/create_schedules.py
@@ -1,16 +1,21 @@
+import csv
 import datetime
+from pathlib import Path
 
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app import crud
 from app.api.exceptions import NameNotFoundException
 from app.main import logger
-from app.models.book import BookEnum
+from app.models.book import Book
+from app.models.passage import PassageCreate
 from app.models.plan import Plan
 from app.models.schedule import ScheduleCreate
 
 YEAR = 2024
-NUM_OF_UNITS_PER_DAY = 2
+NUM_OF_PASSAGES_PER_DAY = 2
+
+source_root = Path("app/initial_data")
 
 
 async def populate_schedules(session: AsyncSession) -> None:
@@ -18,22 +23,42 @@ async def populate_schedules(session: AsyncSession) -> None:
     plan = await crud.plan.get_by_title(session, name)
     if plan is None:
         raise NameNotFoundException(Plan, name)
-    units = await crud.unit.list_by_book_type(
-        session, book_type=BookEnum.NT, limit=None
-    )
-    all_dates = get_dates_of_year(YEAR)
-    index = 0
-    for day in all_dates:
-        if is_weekday(day):
-            daily_units = units[index : index + NUM_OF_UNITS_PER_DAY]
-            index += NUM_OF_UNITS_PER_DAY
-        else:
-            daily_units = []
 
-        schedule = await crud.schedule.create(
-            session, ScheduleCreate(plan_id=plan.id, date=day, units=daily_units)
-        )
-        logger.info("schedule %s created.", schedule)
+    all_dates = get_dates_of_year(YEAR)
+
+    csv_file_path = source_root / "daily_passage.csv"
+    with Path.open(csv_file_path, encoding="utf-8") as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row, day in zip(csv_reader, all_dates, strict=False):
+            if schedule := await crud.schedule.get_by_attr(session, date=day):
+                logger.info("Schedule exists: %s", schedule)
+                continue
+
+            passages = [
+                await get_passage_from_str(session, passage_str)
+                for passage_str in row["verses"].split(";")
+            ]
+            schedule = await crud.schedule.create(
+                session, ScheduleCreate(plan_id=plan.id, date=day, passages=passages)
+            )
+            logger.info("schedule created %s", schedule)
+
+
+async def get_passage_from_str(session: AsyncSession, passage_str: str):
+    *short_name, verses = passage_str.split(" ")
+    short_name_en = " ".join(short_name)
+    book = await crud.book.get_by_attr(session, short_name_en=short_name_en)
+    if book is None:
+        raise NameNotFoundException(Book, short_name_en)
+    if passage := await crud.passage.get_by_attr(
+        session, book_id=book.id, verses=verses
+    ):
+        logger.info("Passage exists: %s", passage)
+    else:
+        data_in = PassageCreate(book_id=book.id, verses=verses)
+        passage = await crud.passage.create(session, data_in)
+        logger.info("Passage created: %s", passage)
+    return passage
 
 
 def get_dates_of_year(year: int) -> list[datetime.date]:
@@ -47,7 +72,3 @@ def get_dates_of_year(year: int) -> list[datetime.date]:
 def is_weekday(day: datetime.date) -> bool:
     """Return True if day is a weekday."""
     return day.isoweekday() in range(1, 6)
-
-
-if __name__ == "__main__":
-    get_dates_of_year(2024)
