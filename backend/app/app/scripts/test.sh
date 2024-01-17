7021ea6ce7a3e2d5de8f63093b437502515dde7d
ChingT <j60626j@gmail.com>
2024-01-17 15:28:14 +0100

    Rename scripts

--- backend/app/app/scripts/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-python -m pytest app/tests -s "${@}"
--- scripts/test.sh
+++ scripts/test.sh
@@ -8,5 +8,5 @@ docker compose -f docker-compose-test.yml config > docker-stack-test.yml
 docker compose -f docker-stack-test.yml build
 docker compose -f docker-stack-test.yml down --remove-orphans
 docker compose -f docker-stack-test.yml up -d
-docker compose -f docker-stack-test.yml exec -T backend bash /app/tests-start.sh "$@"
+docker compose -f docker-stack-test.yml exec -T backend bash /app/tests-prestart.sh "$@"
 docker compose -f docker-stack-test.yml down -v --remove-orphans
