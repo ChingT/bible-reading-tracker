d13aa3e9036d4fedc09a4739318b971033f91b96
ChingT <j60626j@gmail.com>
2024-01-24 14:54:28 +0100

    Redirect to signin if not logged-in when clicking checkbox

--- frontend/src/components/Plan/Schedule.jsx
+++ frontend/src/components/Plan/Schedule.jsx
@@ -1,16 +1,22 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import useApiRequest from "../../hooks/useApiRequest.js";
 
 function ScheduleComponent({ initSchedule, books }) {
+  const isLoggedIn = useSelector((store) => store.loggedInUser.accessToken);
   const { sendRequest, data } = useApiRequest();
   const [schedule, setSchedule] = useState(initSchedule);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (data) setSchedule(data);
   }, [data]);
 
   const handleToggleSelection = (schedule_id) => {
-    sendRequest("patch", `plans/finished_schedule/${schedule_id}`);
+    if (isLoggedIn)
+      sendRequest("patch", `plans/finished_schedule/${schedule_id}`);
+    else navigate("/signin");
   };
 
   const find_book_name = (book_id) => {
